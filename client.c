77327f588b181fb2ae8d939210754468afc20050
perrynzhou <perrynzhou@gmail.com>
2020-07-03 18:37:40 +0800

    nothings

--- client.c
+++ client.c
@@ -6,11 +6,3 @@
  ************************************************************************/
 
 #include <stdio.h>
-
-> File Name : client.c
-    > Author : perrynzhou
-               > Mail : perrynzhou @gmail.com
-               > Created Time : Friday,
-    July 03, 2020 PM02 : 24 : 30 HKT************************************************************************ /
-
-#include <stdio.h>
